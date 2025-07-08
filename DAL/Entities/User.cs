d8db5f5ebe6f4524fadb29d49de49e3300ce1367
Qu <Quanvle@live.dk>
2025-07-08 10:31:00 +0200

    Use favor by compisition over inheritance

---
+++ DAL/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace DAL.Entities;
+
+public class User
+{
+	public int Id { get; set; }
+	public string? FirstName { get; set; }
+	public string? LastName { get; set; }
+	public required IdentityUser IdentityUser { get; set; }
+}
