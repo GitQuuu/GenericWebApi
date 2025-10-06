7ce2c0c69bc9f8da1f521e18610753512ba53315
Qu <qu@peer-performance.com>
2025-10-07 00:54:55 +0200

    Implements authentication orchestration
    Adds an authentication orchestrator to handle the login flow using identity provider and token services.
    
    Refactors and moves identity provider and token services into Authentication folder for better organization.
    
    Adds database changes.

---
+++ Api/Services/Authentication/AuthenticationOrchestrator.cs
@@ -0,0 +1,55 @@
+﻿using System.Security.Claims;
+using Api.Services.IdentityProviderService;
+using Api.Services.TokenService;
+using Microsoft.AspNetCore.Mvc;
+using Services.ResponseService;
+
+namespace Api.Services.Authentication;
+
+/// <summary>
+/// 
+/// </summary>
+public class AuthenticationOrchestrator : IAuthenticationOrchestrator
+{
+	private readonly IIdentityProviderService _identityProviderService;
+	private readonly ITokenService _tokenService;
+	private readonly IHttpContextAccessor _httpContextAccessor;
+	private readonly IResponseService _responseService;
+
+	public AuthenticationOrchestrator(IIdentityProviderService identityProviderService,
+									  ITokenService tokenService,
+									  IHttpContextAccessor httpContextAccessor,
+									  IResponseService responseService)
+	{
+		_identityProviderService = identityProviderService;
+		_tokenService            = tokenService;
+		_httpContextAccessor     = httpContextAccessor;
+		_responseService		 = responseService;
+	}
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <returns></returns>
+	/// <exception cref="NotImplementedException"></exception>
+	public async Task<IActionResult> HandleEntraLoginAsync()
+	{
+		ClaimsPrincipal principal     = _httpContextAccessor.HttpContext?.User;
+		ServiceResult<TokenResponse> entraResponse = await _identityProviderService.ExchangeMicrosoftTokenAsync(principal);
+
+		return await _responseService.HandleResultAsync(entraResponse);
+	}
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <returns></returns>
+	/// <exception cref="NotImplementedException"></exception>
+	public async Task<IActionResult> HandleEntraGoogleAsync()
+	{
+		ClaimsPrincipal              principal     = _httpContextAccessor.HttpContext?.User;
+		ServiceResult<TokenResponse> entraResponse = await _identityProviderService.ExchangeGoogleTokenAsync(principal);
+
+		return await _responseService.HandleResultAsync(entraResponse);
+	}
+}
---
+++ Api/Services/Authentication/IAuthenticationOrchestrator.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Services.Authentication;
+
+public interface IAuthenticationOrchestrator
+{
+	Task<IActionResult> HandleEntraLoginAsync();
+	Task<IActionResult> HandleEntraGoogleAsync();
+}
