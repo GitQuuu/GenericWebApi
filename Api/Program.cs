975cee5f34b1123706b1396c615ef52593393f96
Qu <Quanvle@live.dk>
2025-10-21 02:05:39 +0200

    Configures API project structure
    Configures the Api project file to reference necessary dependencies,
    sets up documentation generation, and removes default content files.
    
    Removes default ASP.NET Core MVC files and configuration, opting
    for a more minimal API structure.

--- Api/Program.cs
+++ Api/Program.cs
@@ -186,8 +186,6 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
-	app.UseExceptionHandler("/Home/Error");
-
 	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
 }
@@ -198,12 +196,6 @@ app.UseRouting();
 app.UseAuthentication(); 
 app.UseAuthorization();
 
-app.MapStaticAssets();
-
-app.MapControllerRoute(
-					   name : "default",
-					   pattern : "{controller=Home}/{action=Index}/{id?}")
-   .WithStaticAssets();
-
+app.MapControllers();
 
 app.Run();
