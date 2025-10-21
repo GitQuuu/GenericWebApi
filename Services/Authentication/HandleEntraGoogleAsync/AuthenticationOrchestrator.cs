fb50c7b2e32f447a909f08f75781887a856dc702
Qu <Quanvle@live.dk>
2025-10-21 02:14:16 +0200

    Improves authentication flow
    Refactors the authentication process to handle potential null
    user data during the Entra/Google token exchange.
    This enhancement ensures a smoother and more reliable authentication
    experience by handling edge cases and returning appropriate
    responses to the client.

--- Services/Authentication/HandleEntraGoogleAsync/AuthenticationOrchestrator.cs
+++ Services/Authentication/HandleEntraGoogleAsync/AuthenticationOrchestrator.cs
@@ -1,6 +1,7 @@
 ﻿using System.Security.Claims;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Services.Authentication.TokenService;
 
 namespace Services.Authentication;
 
@@ -9,9 +10,15 @@ public partial class AuthenticationOrchestrator
 	/// <inheritdoc />
 	public async Task<IActionResult> HandleEntraGoogleAsync(CancellationToken ctx = default)
 	{
-		ClaimsPrincipal             principal     = _httpContextAccessor.HttpContext?.User ?? throw new ArgumentNullException(nameof(principal));
-		ServiceResult<IdentityUser> entraResponse = await _identityProviderService.ExchangeGoogleTokenAsync(principal);
-		var                         tokenResponse = await _tokenService.CreateForUserAsync(entraResponse.Data);
+		ClaimsPrincipal              principal     = _httpContextAccessor.HttpContext?.User ?? throw new ArgumentNullException(nameof(principal));
+		ServiceResult<IdentityUser>  entraResponse = await _identityProviderService.ExchangeGoogleTokenAsync(principal, ctx);
+		
+		if (entraResponse.Data is null)
+		{
+			return await _responseService.HandleResultAsync(entraResponse); 
+		}
+		
+		ServiceResult<TokenResponse> tokenResponse = await _tokenService.CreateForUserAsync(entraResponse.Data);
 
 		return await _responseService.HandleResultAsync(tokenResponse);
 	}
