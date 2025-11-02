37ca1cd35438971d4c8f75e04c20eb2419a13dff
Qu <Quanvle@live.dk>
2025-11-02 01:44:11 +0100

    Removes login functionality from Home Controller
    Removes the Login endpoint and related token generation logic from the Home Controller.
    This functionality is moved to a dedicated authentication service to adhere to separation of concerns, enhance maintainability, and enable a more robust authentication implementation.
    
    The changes involve removing the endpoint responsible for user authentication and JWT token generation, paving the way for a cleaner separation of concerns within the application.

--- Api/Controllers/Home/HomeController.cs
+++ Api/Controllers/Home/HomeController.cs
@@ -1,10 +1,5 @@
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
 
 namespace Api.Controllers.Home;
 
@@ -42,46 +37,6 @@ public class HomeController : ControllerBase
 		return Ok("Protected endpoint authorized");
 	}
 
-	/// <summary>
-	/// Login endpoint to authenticate users and return a JWT token.
-	/// </summary>
-	/// <param name="loginRequest">The login request containing username and password.</param>
-	/// <returns>A JWT token if authentication is successful.</returns>
-	[HttpPost("Login2")]
-	public IActionResult Login([FromBody] TestLoginRequest loginRequest)
-	{
-		var userManager = HttpContext.RequestServices.GetRequiredService<UserManager<IdentityUser>>();
-		var user = userManager.FindByEmailAsync(loginRequest.Email).Result;
-
-		if (user != null && userManager.CheckPasswordAsync(user, loginRequest.Password).Result)
-		{
-			var token = GenerateJwtToken(user.UserName);
-			return Ok(new { Token = token });
-		}
-
-		return Unauthorized("Invalid username or password");
-	}
-
-	private string GenerateJwtToken(string username)
-	{
-		var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:SecretKey"]));
-		var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
-
-		var claims = new[]
-		{
-			new Claim(ClaimTypes.Name, username),
-			new Claim(ClaimTypes.Role, "User")
-		};
-
-		var token = new JwtSecurityToken(
-										 _configuration["Jwt:Issuer"],
-										 _configuration["Jwt:Audience"],
-										 claims,
-										 expires: DateTime.Now.AddHours(1),
-										 signingCredentials: credentials);
-
-		return new JwtSecurityTokenHandler().WriteToken(token);
-	}
 }
 
 /// <summary>
