6b4f247fba616236a899c22cd3bd3d3541414aaf
Qu <Quanvle@live.dk>
2025-11-18 08:32:37 +0100

    Figure out how to return the dto

---
+++ Services/Authentication/HandleGetAllUsersAsync/GetAllUsersResponse.cs
@@ -0,0 +1,20 @@
+﻿namespace Services.Authentication;
+
+public class GetAllUsersResponse
+{
+	public required List<UserDto> Users { get; init; }
+	public required int TotalCount { get; init; }
+
+	public class UserDto
+	{
+		public required string Email { get; init; }
+		public required string UserName { get; init; }
+		public bool EmailConfirmed { get; init; }
+		public string? PhoneNumber { get; init; }
+		public bool PhoneNumberConfirmed { get; init; }
+		public bool TwoFactorEnabled { get; init; }
+		public DateTimeOffset? LockoutEnd { get; init; }
+		public bool LockoutEnabled { get; init; }
+		public int AccessFailedCount { get; init; }
+	}
+}
--- Services/Authentication/HandleGetAllUsersAsync/HandleGetAllUsersAsync.cs
+++ Services/Authentication/HandleGetAllUsersAsync/HandleGetAllUsersAsync.cs
@@ -1,3 +1,5 @@
+using Mapster;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Services.Authentication;
@@ -7,8 +9,7 @@ public partial class AuthenticationOrchestrator
 	/// <inheritdoc />
 	public async Task<IActionResult> HandleGetAllUsersAsync(CancellationToken ct = default)
 	{
-		var result = await _userService.GetAllUsersAsync(ct);
-		
+		var                       result = await _userService.GetAllUsersAsync(ct);
 		return await _responseService.HandleResultAsync(result);
 	}
 }
